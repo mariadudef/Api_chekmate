ffe1185f27bba0cec49fd1c5d4aa25c08452883a
Maria Eduarda <77418873+mariadudef@users.noreply.github.com>
2022-11-12 17:21:46 -0300

    api

---
+++ Api_chekmate/src/Controller/ClienteController.js
@@ -0,0 +1,1 @@
+const ClienteService = require('../services/ClienteService');
---
+++ Api_chekmate/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const ClienteController = require('./Controller/ClienteController');
+
+
+module.exports = router;
